e1c38bc4ff6c8c550d4a28084428fabf64e3a2e6
LuigiBlood <luigiblood@gmail.com>
2021-12-19 14:33:46 +0100

    Change D64 File Generation

--- _build_JPN_D64.bat
+++ _build_JPN_D64.bat
@@ -3,3 +3,4 @@ IF EXIST "EZLJ_Ura.rom" del "EZLJ_Ura.rom"
 IF EXIST "EZLJ_Ura.id" del "EZLJ_Ura.id"
 IF EXIST "EZLJ_Ura.sys" del "EZLJ_Ura.sys"
 bass.exe -d D64=1 ./asm/EZLJ_DISK_Main.asm
+ddmaster -cfg EZLJ_disk.cfg -rom EZLJ_Ura.rom -ipladdr 80000400 -iplsize 197200 -o EZLJ_Ura.d64
--- _build_USA_D64.bat
+++ _build_USA_D64.bat
@@ -3,3 +3,4 @@ IF EXIST "EZLE_Ura.rom" del "EZLE_Ura.rom"
 IF EXIST "EZLE_Ura.id" del "EZLE_Ura.id"
 IF EXIST "EZLE_Ura.sys" del "EZLE_Ura.sys"
 bass.exe  -d D64=1 -d USA=1 ./asm/EZLJ_DISK_Main.asm
+ddmaster -cfg EZLE_disk.cfg -rom EZLE_Ura.rom -ipladdr 80000400 -iplsize 197200 -o EZLE_Ura.d64
